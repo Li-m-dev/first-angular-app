c89c969e1b9830d1f9252110ee64bc4509530038
Li Swafford <liswafford@yahoo.com>
2019-04-18 11:38:55 -0500

    full CRUD

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
+
+import { HeroesComponent } from "./heroes/heroes.component";
+import { MetadataOverrider } from "@angular/core/testing/src/metadata_overrider";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
+// Angular needs to know how the pieces of your application fit together
+// and what other files and libraries the app requires. This information
+// is called Metadata.
+
+/*
+Some of the metadata is in the @Component decorators that you added to your component classes. 
+Other critical metadata is in @NgModule decorators.
+The most important @NgModule decorator annotates the top-levels AppModule class.
+
+
+*/
